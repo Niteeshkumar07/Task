8288a625e9221afe6935fc04fdbe3d634d267c05
Niteeshkumar07 <niteeshkumar4020@gmail.com>
2025-09-17 17:27:12 +0530

    no changes

--- src/main/java/com/example/Task/User.java
+++ src/main/java/com/example/Task/User.java
@@ -1,18 +1,24 @@
 package com.example.Task;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String name;
-    private String email;
-    private String city;
-    private String pincode;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private String email;
+  private String city;
+  private String pincode;
+
 }
--- src/main/java/com/example/Task/UserRepository.java
+++ src/main/java/com/example/Task/UserRepository.java
@@ -1,8 +1,8 @@
 package com.example.Task;
-import com.example.Task.User.*;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
